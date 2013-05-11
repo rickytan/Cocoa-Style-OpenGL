02766c9b820fbe9b469cea9127f769e1cf113104
rickytan <ricky.tan.xin@gmail.com>
2013-05-12 02:33:10 +0800

    lighting works!

--- cocoagl.h
+++ cocoagl.h
@@ -220,6 +220,20 @@ inline void ccGLClear(GLAttribMask mask)
 {
     glClear(mask);
 }
+    
+    /* ReadBufferMode */
+    /*      GL_FRONT_LEFT */
+    /*      GL_FRONT_RIGHT */
+    /*      GL_BACK_LEFT */
+    /*      GL_BACK_RIGHT */
+    /*      GL_FRONT */
+    /*      GL_BACK */
+    /*      GL_LEFT */
+    /*      GL_RIGHT */
+    /*      GL_AUX0 */
+    /*      GL_AUX1 */
+    /*      GL_AUX2 */
+    /*      GL_AUX3 */
 
 CC_ENUM(int, GLDrawMode) {
 	GLDrawModePoints =  GL_POINTS,
--- test/MacOS/CCgl/main.cpp
+++ test/MacOS/CCgl/main.cpp
@@ -25,23 +25,37 @@ int _winW = 800, _winH = 600;
 
 const double fovy = 45.0, zNear = 0.5, zFar = 10.0;
 
+GLfloat _rotX = 30.f;
+GLfloat _rotY = 45.f;
+GLfloat _rotZ = 0.f;
+
 void init()
 {
-    glClearDepth(1.0);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(1.0f);
+    glClearColor(0.3f, 0.3f, 0.3f, 0.3f);
+    
     ccGLEnable(GLSwitchDepthTest);
     ccGLEnable(GLSwitchLighting);
+    //ccGLEnable(GLSwitchNormalize);
+    //ccGLEnable(GLSwitchCullFace);
+    glEnable(GL_LIGHT0);
     
-    static GLfloat ambient_light[] = {1.0, 1.0, 1.0, 1.0};
-    static GLfloat diffuse_light[] = {0.8, 0.8, 1.0, 1.0};
-    static GLfloat light_pos[] = {0.0, 2.0, 5.0, 1.0};
-    static GLfloat light_direction[] = {0.0, 0.0,-1.0};
+    ccGLFrontFace(GLFrontFaceDirectionCounterClockWiseIsFront);
+    
+    static GLfloat ambient_light[] = {0.6, 0.6, 0.6, 1.0};
+    static GLfloat diffuse_light[] = {0.4, 0.4, 1.0, 1.0};
+    static GLfloat specular_light[] = {0.3, 0.0, 0.0, 1.0};
+    static GLfloat light_pos[] = {1.0, 3.0, 1.0, 0.0};
     
     ccGLLight(GLLight0, GLLightParamAmbientfv4, ambient_light);
     ccGLLight(GLLight0, GLLightParamDiffusefv4, diffuse_light);
+    //ccGLLight(GLLight0, GLLightParamSpecularfv4, specular_light);
     ccGLLight(GLLight0, GLLightParamPositionfv4, light_pos);
-    ccGLLight(GLLight0, GLLightParamSpotCutofff, 30.0);
-    ccGLLight(GLLight0, GLLightParamSpotDirectionfv3, light_direction);
+//    ccGLLight(GLLight0, GLLightParamConstantAttenuationf, 2.0);
+//    ccGLLight(GLLight0, GLLightParamLinearAttenuationf, .2);
+//    ccGLLight(GLLight0, GLLightParamQuadraticAttenuationf, 2.0);
+    
+//    ccGLLightModel(GLLightModelAmbientfv3, ambient_light);
 }
 
 void onDraw()
@@ -51,13 +65,20 @@ void onDraw()
     ccGLMatrixMode(GLMatrixModeModelView);
     glLoadIdentity();
     
-    gluLookAt(0, 0, 4, 0, 0, 0, 0, 1, 0);
-    
-    static GLfloat ambient_light[] = {0.9, 0.9, 0.9, 0.9};
-    static GLfloat diffuse_light[] = {0.8, 0.8, 0.8, 0.8};
+    static GLfloat ambient_light[] = {0.6, 0.6, 0.6, 0.6};
+    static GLfloat diffuse_light[] = {0.6, 0.6, 0.8, 0.8};
+    static GLfloat specular_light[] = {0.4, 0.0, 0.0, 1.0};
     
     ccGLMaterial(GLFaceFront, GLLightParamAmbientfv4, ambient_light);
     ccGLMaterial(GLFaceFront, GLLightParamDiffusefv4, diffuse_light);
+    //ccGLMaterial(GLFaceFront, GLLightParamSpecularfv4, specular_light);
+    
+    //ccGLPolygonMode(GLFaceFrontAndBack, GLPolygonModeLine);
+    
+    glTranslatef(0, 0, -4);
+    glRotatef(_rotX, 1.0, 0, 0);
+    glRotatef(_rotY, 0, 1.0, 0);
+    glRotatef(_rotZ, 0, 0, 1.0);
     
     glutSolidTeapot(1.0);
     
@@ -82,10 +103,27 @@ void onKeyborad(unsigned char key, int x, int y)
         case 27:
             exit(0);
             break;
-            
+        case 'a':
+            _rotY -= 5.0;
+            break;
+        case 'd':
+            _rotY += 5.0;
+            break;
+        case 's':
+            _rotX += 5.0;
+            break;
+        case 'w':
+            _rotX -= 5.0;
+            break;
         default:
             break;
     }
+    glutPostRedisplay();
+}
+
+void onIdle()
+{
+    glutPostRedisplay();
 }
 
 int main(int argc, char *argv[])
@@ -93,12 +131,13 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(_winW, _winH);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     
     glutCreateWindow("Test cocoaGL");
     glutDisplayFunc(onDraw);
     glutReshapeFunc(onResize);
     glutKeyboardFunc(onKeyborad);
+    //glutIdleFunc(onIdle);
     
     init();
     
